373d7017739b9ccbcd96afa656a89cfc6e677735
acidicnic <nic4096@gmail.com>
2020-07-15 18:07:31 -0700

    README updated and scrape.go completed

--- README.md
+++ README.md
@@ -1,64 +1,74 @@
-# 🕷 makescraper
+# 🕷 Reddit WebScraper
 
-[![Go Report Card](https://goreportcard.com/badge/github.com/acidicnic/makescraper)](https://goreportcard.com/report/github.com/acidicnic/makescraper)
-
-_Create your very own web scraper and crawler using Go and [Colly](https://go-colly.org)!_
+[![Go Report Card](https://goreportcard.com/badge/github.com/acidicnic/redditWebScraper)](https://goreportcard.com/report/github.com/acidicnic/redditWebScraper)
 
 ### 📚 Table of Contents
 
 1. [Project Structure](#project-structure)
 2. [Getting Started](#getting-started)
-3. [Deliverables](#deliverables)
-4. [Resources](#resources)
+3. [Flags](#flags)
+4. [Output](#output.json)
 
 ## Project Structure
 
 ```bash
-📂 makescraper
+📂 redditWebScraper
 ├── README.md
 └── scrape.go
 ```
 
 ## Getting Started
 
-...
+```bash
+git clone https://github.com/AcidicNic/redditWebScraper.git
+cd redditWebScraper
+go run scrape.go
+```
 
-## Deliverables
+## Flags
 
-### Requirements
 
-#### Scraping
+```bash
+// This will get the 10 newest posts from /r/TurnipExchange, excluding posts with the flair CLOSED
+go run scrape.go -subreddit="TurnipExchange/new" -lmt=10 -filter="CLOSED"
+```
 
-- [ ] **IMPORTANT**: Complete the Web Scraper Workflow worksheet distributed in class.
-- [ ] Create a `struct` to store your data.
-- [ ] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
-- [ ] Print the data you scraped to `stdout`.
+- __subreddit__ _string_
+    - The name of a subreddit and category if you'd like. (_default "TurnipExchange/new"_)
+- __filter__ _string_
+	- A flair you'd like to exclude from the results.
+- __lmt__ _int_
+	- How many posts off the first page you want returned.
 
-##### Stretch Challenges
 
-- [ ] Add more fields to your `struct`. Extract multiple data points from the website. Print them to `stdout` in a readable format.
+## output.json
+```json
+[
+	{
+        /* Post title */
+		"title": "This is the title of a super interesting post!",
 
-#### Serializing & Saving
+        /* Direct link to the post */
+		"postUrl": "https://www.reddit.com/r/<subreddit>/comments/<post ID>/<post_slug>/",
 
-- [ ] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
-- [ ] Write scraped data to a file named `output.json`.
-- [ ] **Add, commit, and push to GitHub**.
+        /* Time the post was uploaded to Reddit */
+		"time": "Thu Jul 16 00:45:14 2020 UTC",
 
-## Resources
+        /* Author's username */
+		"author": "RedditUser31498234",
 
-### Lesson Plans
+        /* Number of comments */
+		"comments": 0,
 
-- [**BEW 2.5** - Scraping the Web](https://make-school-courses.github.io/BEW-2.5-Strongly-Typed-Languages/#/Lessons/WebScraping.md): Concepts and examples covered in class related to web scraping and crawling.
+        /* Linked URL (Empty String for text posts) */
+		"source": "",
 
-### Example Code
-
-#### Scraping
-
-- [**Colly** - Docs](http://go-colly.org/docs/): Check out the sidebar for 20+ examples!
-- [**Ali Shalabi** - Syntax-Helper](https://github.com/alishalabi/syntax-helper): Command line interface to help generate proper code syntax, pulled from the Golang documentation.
-
-#### Serializing & Saving
-
-- [JSON to Struct](https://mholt.github.io/json-to-go/): Paste any JSON data and convert it into a Go structure that will support storing that data.
-- [GoByExample - JSON](https://gobyexample.com/json): Covers Go's built-in support for JSON encoding and decoding to and from built-in and custom data types (structs).
-- [GoByExample - Writing Files](https://gobyexample.com/writing-files): Covers creating new files and writing to them.
+        /* Flair (Empty String for unflaired posts) */
+		"flair": ""
+	},
+	{
+        ...
+    },
+    ...
+]
+```
--- scrape.go
+++ scrape.go
@@ -1,46 +1,128 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/gocolly/colly"
 	"io/ioutil"
+	"strconv"
+	"encoding/json"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
+    subreddit := flag.String("subreddit", "TurnipExchange/new",
+		"The name of a subreddit and category if you'd like.")
+	lmt := flag.Int("lmt", 0,
+		"How many posts off the first page you want returned.")
+	filterFlair := flag.String("filter", "",
+		"A flair you'd like to exclude from the results.")
+	flag.Parse()
+
+	posts := scrapeReddit(*subreddit, *lmt, *filterFlair)
+
+	prettyPrintPosts(posts)
+
+	savePostsToJson(posts)
+}
+
+type post struct {
+	Title string `json:"title"`
+    PostURL string `json:"postUrl"`
+    Time string `json:"time"`
+    Author string `json:"author"`
+    Comments int `json:"comments"`
+    Source string `json:"source"`
+    Flair string `json:"flair"`
+}
+
+func scrapeReddit(subreddit string, limit int, filterFlair string) []post {
 	var posts []post
 	c := colly.NewCollector()
 
-	c.OnHTML("a.title", func(e *colly.HTMLElement) {
-		link := getURL(e.Attr("href"))
+	c.OnHTML("div.top-matter", func(e *colly.HTMLElement) {
+		flair := e.ChildAttr("span.linkflairlabel", "title")
 
-		posts = append(posts, post{Title: e.Text, URL: link})
+		if filterFlair != "" && flair != filterFlair {
+			posts = append(posts, post{
+				Title: e.ChildText("a.title"),
+				PostURL: oldUrlToNew(e.ChildAttr("a.comments", "href")),
+				Flair: flair,
+				Time: e.ChildAttr("time.live-timestamp", "title"),
+				Author: e.ChildText("a.author"),
+				Comments: getCommentNum(e.ChildText("a.comments")),
+				Source: getLinkedURL(e.ChildAttr("a.title", "href")),
+			})
+		}
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
+	subreddit = strings.Trim(subreddit, "/")
+	c.Visit(fmt.Sprintf("https://old.reddit.com/r/%s/", subreddit))
 
-	c.Visit("https://old.reddit.com/r/TurnipExchange/new/")
-
-	text := ""
-	for _, post := range posts {
-		text += fmt.Sprintf("%s \n\t%s\n\n", post.Title, post.URL)
-		fmt.Printf("%s \n\t%s\n\n", post.Title, post.URL)
+	if len(posts) > limit && limit > 0 {
+		return posts[:limit]
 	}
-	byte_text := []byte(text)
-    err := ioutil.WriteFile("turnip_prices", byte_text, 0644)
-	if err != nil {
-        panic(err)
-    }
+	fmt.Println(len(posts))
+	return posts
 }
-func getURL(url string) string {
-	if url[0:3] == "/r/" {
-		return "https://www.reddit.com" + url
+
+func getCommentNum(commentText string) int {
+	// No comments
+	if commentText == "comment" {
+		return 0
 	}
+	// only 1 comment
+	if commentText == "1 comment" {
+        return 1
+    }
+	// commentsText will be "(some int) comments" so return the int
+	CommentNum, err := strconv.Atoi(commentText[:len(commentText)-9])
+    checkErr(err)
+	return CommentNum
+}
+
+func oldUrlToNew(url string) string {
+	// replaces 'old' with 'www'
+	// 'https://old.reddit.com/...' --> 'https://www.reddit.com/...'
+	return fmt.Sprintf("%swww%s", url[:8], url[11:])
+}
+
+func getLinkedURL(url string) string {
+	// if this is a text post, return empty string
+	if url[:3] == "/r/" {
+		return ""
+	}
+	// if there is a link return the link
 	return url
 }
 
-type post struct {
-	Title string
-    URL string
+func prettyPrintPosts(posts []post) {
+	for _, post := range posts {
+		postText := fmt.Sprintf(
+			"%s\n\tPost URL: %s\n\tTime: %s\n\tAuthor: /u/%s\n\tComments: %d",
+			post.Title, post.PostURL, post.Time, post.Author, post.Comments)
+		if post.Source != "" {
+			postText += "\n\tSource: " + post.Source
+		}
+		if post.Flair != "" {
+			postText += "\n\tFlair: " + post.Flair
+		}
+		fmt.Println(postText + "\n")
+	}
+}
+
+func savePostsToJson(posts []post) {
+	postsJson, err := json.MarshalIndent(posts, "", "	")
+	checkErr(err)
+	ioutil.WriteFile("output.json", postsJson, 0644)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
